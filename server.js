e1a6960c16ea0d152079e886ae462abf43ad4137
Amanda <amanda.alves15@gmail.com>
2016-07-05 16:52:23 +0000

    commit

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-var express = require("express");
-var app = express();
-var port = process.env.port || 8080;
+'use strict';
 
+
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+
+var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
 
 app.get( function(req, res) {
     var conf = (req.headers['user-agent']).substring(13, 30);
@@ -15,6 +42,9 @@ app.get( function(req, res) {
     }, null, 3));
 });
 
-app.listen(port, function() {
-    console.log("I'm serving your desires.");
-})
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
+
