cb34c6381fb04d21f522b30cb774e15ae7d48cf9
Amanda <amanda.alves15@gmail.com>
2016-07-05 22:57:00 +0000

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var routes = require("./routes.js");
 var express = require("express");
 var app = express();
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 routes(app);
 
